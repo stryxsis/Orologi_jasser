246a14f023828e6aeb28913e6a3d58a55ba0bf3a
Vlad Costin <vladcostin58@gmail.com>
2025-01-03 18:17:19 +0100

    fatto

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,5 +1,4 @@
 const btnMenu = document.getElementById("btn-hamburger");
-<<<<<<< HEAD
 const svgMenu = document.getElementById("svg-hamburger");
 
 btnMenu.addEventListener("click", () => {
@@ -11,10 +10,4 @@ btnMenu.addEventListener("click", () => {
     navMenu.classList.add("nav_menu-show");
     svgMenu.style.rotate = "90deg";
   }
-=======
-
-btnMenu.addEventListener("click", () => {
-  const navMenu = document.getElementById("nav-menu");
-  navMenu.classList.toggle("nav_menu-show");
->>>>>>> 910ea8af39682a75a77e2c5183657fd0c796d8a2
 });
